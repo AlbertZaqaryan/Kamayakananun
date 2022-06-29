c4f708013cdd82416dca21913ae44c98076666de
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2022-06-29 16:19:39 +0400

    Tnayin1

--- less.py
+++ less.py
@@ -1,1 +1,31 @@
-print('Hello')
+# Exercise 110: Sorted Order
+# user_input = int(input("Enter values: "))
+# mylist = []
+# while True:
+#     user_input = int(input("Enter values: "))
+#     if user_input != 0:
+#         mylist.append(user_input)
+#     else:
+#         print(mylist)
+#         break
+# -------------------------------------------------------
+# Exercise 111: Reverse Order??
+# user_input = int(input("Enter values: "))
+# mylist = []
+# while True:
+#     user_input = int(input("Enter values: "))
+#     if user_input != 0:
+#         mylist.append(user_input)
+#     else:
+#         print(mylist)
+#         break
+# print(mylist, reversed = True)
+# -------------------------------------------------------
+# word = input('Enter words--> ')
+# mylist = []
+# while word != "":
+#     if word not in mylist:
+#         mylist.append(word)
+    
+#     for word in mylist:
+#         print(word)
